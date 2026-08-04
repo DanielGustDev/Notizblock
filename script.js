106e6521afd49b89eb244823f25dfc42571323ff
Daniel_Gust <d.gust@hotmail.com>
2026-08-04 13:31:20 +0200

    added fonts, first img and functions to render content

---
+++ script.js
@@ -0,0 +1,20 @@
+// @ts-nocheck
+
+let notes = ["dog", "cat", "designs"];
+
+function renderNotes() {
+  let contentRef = document.getElementById("content");
+  contentRef.innerHTML = "";
+  for (let indexNote = 0; indexNote < notes.length; indexNote++) {
+    const note = notes[indexNote];
+    contentRef.innerHTML += getNoteTemplate(note);
+  }
+}
+
+function getNoteTemplate(note) {
+  return `<p>+ ${note}</p>`;
+}
+
+// Notizen hinzufügen
+// Notizen löschen
+// Notizen archivieren
