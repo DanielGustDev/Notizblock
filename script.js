ec3ada227874e633491c5f245f9c4b4d8f04fa5e
Daniel_Gust <d.gust@hotmail.com>
2026-08-05 13:11:00 +0200

    added delete function, uodated styling

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // @ts-nocheck
 
+let notesTitles = ["Einkauf", "Aufgabe"];
 let notes = ["dog", "cat", "designs"];
+
+let trashNotesTitles = [];
+let trashNotes = [];
 
 function renderNotes() {
   let contentRef = document.getElementById("content");
@@ -10,21 +14,50 @@ function renderNotes() {
   }
 }
 
+function renderTrashNotes() {
+  let trashcontentRef = document.getElementById("trash_content");
+  trashcontentRef.innerHTML = "";
+  for (
+    let indexTrashNote = 0;
+    indexTrashNote < trashNotes.length;
+    indexTrashNote++
+  ) {
+    trashcontentRef.innerHTML += getTrashNoteTemplate(indexTrashNote);
+  }
+}
+
 function getNoteTemplate(indexNote) {
-  return `<p>+ ${notes[indexNote]} <button onclick="deleteNote(${indexNote})">X</button></p>`;
+  return `<p>+ title: ${notesTitles[indexNote]} -> ${notes[indexNote]} <button onclick="moveToTrash(${indexNote})">X</button></p>`;
+}
+
+function getTrashNoteTemplate(indexTrashNote) {
+  return `<p>+ ${trashNotesTitles[indexTrashNote]} -> ${trashNotes[indexTrashNote]} <button onclick="deleteNote(${indexTrashNote})">X</button></p>`;
 }
 
 function addNote() {
+  let noteTitleInputRef = document.getElementById("note_title_input");
   let noteInputRef = document.getElementById("note_input");
-  let noteInput = noteInputRef.value;
-  notes.push(noteInput);
+
+  notesTitles.push(noteTitleInputRef ? noteTitleInputRef.value : "Ohne Titel");
+  notes.push(noteInputRef.value);
   renderNotes();
   noteInputRef.value = "";
 }
 
-function deleteNote(indexNote) {
-  notes.splice(indexNote, 1);
+function moveToTrash(indexNote) {
+  let trashNote = notes.splice(indexNote, 1);
+  trashNotes.push(trashNote[0]);
+  let trashNoteTitle = notesTitles.splice(indexNote, 1);
+  trashNotesTitles.push(trashNoteTitle[0]);
   renderNotes();
+  renderTrashNotes();
+}
+
+function deleteNote(indexTrashNote) {
+  trashNotes.splice(indexTrashNote, 1);
+  trashNotesTitles.splice(indexTrashNote, 1);
+
+  renderTrashNotes();
 }
 
 // Notizen archivieren
